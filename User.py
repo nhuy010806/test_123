bf97278d10bef75215cff5282cfcfe26f38175a5
nhuy010806 <huytcnk24406h@st.uel.edu.vn>
2025-03-12 01:54:02 +0700

    Delete User.py

--- User.py
+++
@@ -1,8 +0,0 @@
-class Employee:
-    def __init__(self, EmployeeId,EmployeeName,UserName, Password):
-        self.EmployeeId=EmployeeId
-        self.EmployeeName=EmployeeName
-        self.UserName=UserName
-        self.Password=Password
-    def __str__(self):
-        return f"{self.EmployeeId}\t{self.EmployeeName}"
