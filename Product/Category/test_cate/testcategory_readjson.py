fc9dc690570a885c89bb4b2881bf054ec2f30cf8
pkhanhz <pkhanhz@users.noreply.github.com>
2025-03-22 21:06:17 +0700

    pro&cate

---
+++ Product/Category/test_cate/testcategory_readjson.py
@@ -0,0 +1,10 @@
+from Category.libs_cate.JsonFileFactory import JsonFileFactory
+from Category.models_cate.Category import Category
+
+categories=[]
+jff=JsonFileFactory()
+filename= "../dataset_cate/categories.json"
+categories=jff.read_data(filename,Category)
+print("List of Categories after loading Json: ")
+for cate in categories:
+    print(cate)
---
+++ Product/Product/libs_product/DataConnector.py
@@ -0,0 +1,52 @@
+from Category.models_cate.Category import Category
+from Product.libs_product.JsonFileFactory import JsonFileFactory
+from Product.models_product.Product import Product
+
+
+class DataConnector:
+    def get_all_categories(self):
+        categories = []
+        jff = JsonFileFactory()
+        filename = "../../Category/dataset_cate/categories.json"
+        categories = jff.read_data(filename, Category)
+        return categories
+    def get_all_cateids(self):
+        cateids = []
+        jff = JsonFileFactory()
+        filename = "../../Category/dataset_cate/categories.json"
+        categories = jff.read_data(filename, Category)
+        for category in categories:
+            cateids.append(category.cateid)
+        return cateids
+    def get_all_products(self):
+        products = []
+        jff = JsonFileFactory()
+        filename = "../dataset_product/products.json"
+        products = jff.read_data(filename, Product)
+        return products
+    def get_products_by_categories(self,cateid):
+        products = self.get_all_products()
+        result=[]
+        for product in products:
+            if product.cateid==cateid:
+                result.append(product)
+        return result
+
+    def find_index_product(self,proid):
+        self.products = self.get_all_products()
+        for i, product in enumerate(self.products):
+            if product.proid == proid:
+                return i
+        return -1
+
+    def delete_product(self,proid):
+        index=self.find_index_product(proid)
+        if index!=-1:
+            self.products.pop(index)
+            jff = JsonFileFactory()
+            filename = "../dataset_product/products.json"
+            jff.write_data(self.products, filename)
+
+    def get_products_by_date(self, selected_date):
+        pass
+
