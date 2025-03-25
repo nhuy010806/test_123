cae491d579414000f9811fe8db20a24377c025b1
thaoS2 <ngothiphuongthaoa075@gmail.com>
2025-03-25 16:31:45 +0700

    CategoryMainWindowExt.py

--- ProjectISM/uiCate/CategoryMainWindowExt.py
+++ ProjectISM/uiCate/CategoryMainWindowExt.py
@@ -121,7 +121,7 @@ class ProductMainWindow1Ext(QMainWindow, Ui_MainWindow):
         self.set_buttons_enabled(True)
 
     def open_help(self):
-        file_help = "Hướng dẫn sử dụng category.pdf"
+        file_help = "HELP CATEGORY.pdf"
         current_path = os.getcwd()
         file_help = f"{current_path}/../asset/{file_help}"
         webbrowser.open_new(file_help)
